d82d5433d15dd56e33081cee9b7855c2d6f0b11a
admin <raina.thomas@zetaris.com>
2023-06-08 11:44:35 +1000

    Edited files

--- python-connectors/zetaris-query-result/connector.py
+++ python-connectors/zetaris-query-result/connector.py
@@ -12,7 +12,7 @@ Note: the name of the class itself is not relevant.
 """
 import logging
 from numpy import isnan
-from zstr_session import ZstrSession, get_base_url
+from zstr_session import ZstrSession
 
 import dataiku
 from dataiku.connector import Connector
