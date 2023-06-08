513bdf78209faa89af8986fa05fb360ff020ae89
admin <raina.thomas@zetaris.com>
2023-06-08 12:55:23 +1000

    Edited files

--- python-connectors/zetaris-query-result/connector.py
+++ python-connectors/zetaris-query-result/connector.py
@@ -75,7 +75,7 @@ class CustomExporter(Connector):
 
     def generate_rows(self, dataset_schema=None, dataset_partitioning=None,
                       partition_id=None, records_limit=-1 , results =None):
-            yield self.results
+            yield self.results[]
 
     def get_writer(self, dataset_schema=None, dataset_partitioning=None,
                          partition_id=None):
