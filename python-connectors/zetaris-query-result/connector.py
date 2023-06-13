0394af41e90cbbf933942eb210c4ae417191046b
admin <raina.thomas@zetaris.com>
2023-06-13 10:12:07 +1000

    Edited files

--- python-connectors/zetaris-query-result/connector.py
+++ python-connectors/zetaris-query-result/connector.py
@@ -44,7 +44,7 @@ class CustomExporter(Connector):
         self.base_url = config["zetaris_api"].get("server_url", None)
         self.username = config["zetaris_api"].get("username", None)
         self.password = config["zetaris_api"].get("password", '')
-        self.pageLimit =config["zetaris_api"].get("pageLimit", None)
+        self.pageLimit =self.config.get("pageLimit", "")
         Z = ZstrSession(self.base_url, self.username, self.password)
         self.results = Z.execute_select(self.QUERY, self.pageLimit) 
 
