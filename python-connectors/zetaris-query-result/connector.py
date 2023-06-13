d0e46cd807bd02ab77b73e23f18a033a950605be
admin <raina.thomas@zetaris.com>
2023-06-13 12:01:51 +1000

    Edited files

--- python-connectors/zetaris-query-result/connector.py
+++ python-connectors/zetaris-query-result/connector.py
@@ -44,6 +44,7 @@ class CustomExporter(Connector):
         self.base_url = config["zetaris_api"].get("server_url", None)
         self.username = config["zetaris_api"].get("username", None)
         self.password = config["zetaris_api"].get("password", '')
+        print('This is above the record limit')
         self.recordLimit =self.config.get("recordLimit", "")
         print('This is the record limit' + self.recordLimit)
 
